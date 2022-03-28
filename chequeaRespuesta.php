d41e90ebb7b83a347b0a03d25a143d6edfd3eb73
Jorge Cisneros <fraxito@gmail.com>
2022-03-28 20:16:02 +0200

    Con un PREPARED STATEMENT en la query de

--- chequeaRespuesta.php
+++ chequeaRespuesta.php
@@ -1,3 +1,28 @@
 <?php
+    include('misfunciones.php');
+    $mysqli = conectaBBDD();
 
+    $respuesta = $_POST['respuesta'];
+    $numeroPregunta = $_POST['numeroPregunta'];
+////query mal hecha!!!! SUSPENSO GARANTIZADO!!!!
+ //   $consulta = $mysqli -> query("SELECT * FROM `preguntas` WHERE numero = '$numeroPregunta' ");
+ //   $r = $consulta -> fetch_array();
+
+
+ //query correcta:
+ $consulta = $mysqli -> prepare("SELECT correcta FROM `preguntas` WHERE numero = ? ");
+  
+ $consulta -> bind_param("s", $numeroPregunta);
+ $consulta -> execute();
+ $consulta -> store_result();  
+ $consulta -> bind_result($correcta);
+ $consulta -> fetch();
+
+
+    if ( $correcta == $respuesta){
+        echo 'ACERTASTE!!!';
+    }
+    else{
+        echo 'INCORRECTA LUSER!!!';
+    }
 ?>
